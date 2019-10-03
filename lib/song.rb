69d40b816adf76369a3bff47a0e54ec7055ebce6
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-03 22:43:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,6 +24,16 @@ class Song
     @@artists.uniq
   end
 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count|artist|
+        artist_count|artist| += 1
+      else
+        artist_count|artist| = 1
+      end
+    end 
+
   def self.genres
     @@genres.uniq
   end
